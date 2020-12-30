9ca409e92e9e847caa479116db3f544957d85caa
bmp0924 <31146004+bmp0924@users.noreply.github.com>
2020-12-30 14:42:31 -0800

    Code necessary for frontend implementation
    Javascript file needs jquery to work with API functions

---
+++ EnvBenefits.js
@@ -0,0 +1,18 @@
+$.getJSON('https://monitoringapi.solaredge.com/site/807090/envBenefits?systemUnits=Imperial&api_key=DGOC7JJM5HFA27892BEP9Y64EYVFD9PZ&callback=?' , function(data)
+          
+  {
+  console.log( "success" );
+  console.log(data);
+
+  $.each(data,function(index,value){
+    console.log(value);
+      var trees = value.treesPlanted;
+      var co2 = value.gasEmissionSaved.co2;
+      var units = value.gasEmissionSaved.units;
+
+    
+    $('.trees').text(trees);
+    $('.c02').text(co2 + ' '+ units+ 's');
+    // $('.output').append()
+  });
+});
